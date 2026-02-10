f96a21f5698f4ffc9dbf9ae38075cf2f84570c1e
ahmedRamadan <ahoooamah6@gamil.com>
2026-02-10 17:36:52 +0200

    fix the layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
-
+import { SideBar } from "./layout/SideBar";
+import { Header } from "./layout/Header";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-
+            
+        <>
+          <SideBar/>
+          <Header/>
+        </>
     </div>
   );
 }
